9db47e56eb56c3537f515439f1d2272fa51cf831
tccmw <jungmwoo6248@gmail.com>
2025-12-05 19:39:53 +0900

    feat(#19) : png 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+export const IMAGES = {
+    logo: require('./logo.png'),
+    alarm: require('./alarm.png'),
+    alarmRed: require('./alarm-red.png'),
+    home: require('./home.png'),
+    homeBlack: require('./home-black.png'),
+    MyPage: require('./my-page.png'),
+    MyPageBlack: require('./my-page-black.png'),
+    Task: require('./task.png'),
+    TaskBlack: require('./task-black.png'),
+} as const;
