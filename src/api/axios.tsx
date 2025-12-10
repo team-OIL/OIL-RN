e1be722221d5cb9519a295dd3b018bc47df94e19
tccmw <jungmwoo6248@gmail.com>
2025-12-11 00:56:44 +0900

    refartor(#45) : 파일 이름 변경

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import config from 'react-native-config';
 
-export const axiosInstance = axios.create({
+export const api = axios.create({
   baseURL: config.API_BASE_URL,
   timeout: 1000,
 });
