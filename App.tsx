a74cf28e2a45862138d2be487e09ca708c0589ff
tccmw <jungmwoo6248@gmail.com>
2025-12-14 18:08:32 +0900

    feat(#62) : splash 화면 구현

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BottomTabNavigator from './src/navigation/BottomTabNavigator';
-import { useState } from 'react';
 import { RootStackParamList } from './types/navigation';
 import SignIn from './src/pages/auth/SignIn';
 import SignUp from './src/pages/auth/SignUp';
@@ -11,6 +11,7 @@ import SignInComplete from './src/pages/auth/SignInComplete';
 import AlarmSettings from './src/pages/auth/AlarmSettingsPage';
 import NicknamePage from './src/pages/auth/NicknamePage';
 import AlarmPage from './src/pages/main/AlarmPage';
+import RNBootSplash from 'react-native-bootsplash';
 
 export type LoggedInParamList = {
   Orders: undefined;
@@ -23,14 +24,16 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
+  useEffect(() => {
+    RNBootSplash.hide({ fade: true });
+  }, []);
+
   console.log('App 렌더링됨');
   const MyTheme = {
     ...DefaultTheme,
     colors: { ...DefaultTheme.colors, background: '#fff' },
   };
 
-  const [isTaskStarted, setIsTaskStarted] = useState(true);
-  const [isLoggedIn, setLoggedIn] = useState(false);
   return (
     <NavigationContainer theme={MyTheme}>
       <Stack.Navigator id="stack">
